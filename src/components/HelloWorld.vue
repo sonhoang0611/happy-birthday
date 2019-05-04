c7a29bf4654a13a3394323292b056308ec345e07
sonhoang0611 <hoangthaison985@gmail.com>
2019-05-04 22:39:48 +0700

    Add images and function navigate between images

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,16 @@
 <template>
 <div class="main-app">
-  <div class="carousell container">
-    <div class="image-box">
-      <img src="../assets/img/hpbd.gif" class="img-fluid">
+  <div class="carousell">
+    <div id="prevImg" @click.prevent="prevImage"><</div>
+    <div class="container">
+      <div class="image-box">
+        <img id="image" :src="firstImg.src" class="img-fluid">
+      </div>
+      <div id="speech" class="speech">
+        <span>{{firstImg.text}}</span>
+      </div>
     </div>
+    <div id="nextImg" @click.prevent="nextImage">></div>
   </div>
 </div>
 </template>
@@ -11,8 +18,52 @@
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data () {
+    return {
+      firstImg: {
+          src: require('../assets/hpbd.gif'),
+          text: 'Sinh nhật vui vẻ nghen!!!'
+        },
+      listImgs: [
+        {
+          src: require('../assets/hpbd.gif'),
+          text: 'Sinh nhật vui vẻ nghen!!!'
+        },
+        {
+          src: require('../assets/hpbd2.gif'),
+          text: 'Sinh nhật vui vẻ nghen 2!!!'
+        },
+        {
+          src: require('../assets/db_pig_nose.png'),
+          text: 'Sinh nhật vui vẻ nghen 3!!!'
+        },
+        {
+          src: require('../assets/ravee.gif'),
+          text: 'Sinh nhật vui vẻ nghen 4!!!'
+        }
+      ]
+    }
+  },
+  methods: {
+    nextImage(event) {
+      event.stopPropagation();
+      const self = this;
+      for (let i = 0, len = this.listImgs.length; i < len; i++){
+        if (this.firstImg.src === this.listImgs[i].src) {
+          let j = ++i;
+          Object.assign(this.firstImg, this.listImgs[j % len]);
+        }
+      }
+    },
+    prevImage(event) {
+      event.stopPropagation();
+      for (let len = this.listImgs.length, i = len - 1; i > -1; i--){
+        if (this.firstImg.src === this.listImgs[i].src) {
+          let j = (i % len === 0) ? (len - 1) : (--i);
+          Object.assign(this.firstImg, this.listImgs[j]);
+        }
+      }
+    }
   },
   mounted() {
 
@@ -26,8 +77,8 @@ export default {
   height: 100%;
   width: 100%;
   margin: 0;
-  background: url(../assets/img/birthday-cake.jpg);
-  position: 50% 50%;
+  background: url(../assets/birthday-cake.jpg);
+  background-position: 50% 50%;
   background-size: cover;
   padding: 60px;
   display: flex;
@@ -36,12 +87,49 @@ export default {
 
   .carousell {
     height: 100%;
-    max-width: 825px;
-    padding: 30px;
-    border-radius: 8px;
-    background: white;
-    box-shadow: 1px 1px 15px 1px rgba(0,0,0,0.1);
-    border-color: #eee;
+    display: flex;
+
+    .container {
+      padding: 30px;
+      max-width: 825px;
+      border-radius: 8px;
+      background: white;
+      box-shadow: 1px 1px 15px 1px rgba(0,0,0,0.1);
+      border-color: #eee;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .image-box {
+      max-width: 800px;
+      img {
+        border: 1px solid #ede;
+        border-radius: 6px;
+      }
+    }
+    .speech {
+      display: flex;
+      flex: 1;
+      justify-content: center;
+      align-items: center;
+      font-family: 'Itim', cursive;
+
+      span {
+        font-size: 2em;
+        font-weight: 600;
+      }
+    }
+
+    #prevImg, #nextImg {
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      flex: 1;
+      min-width: 40px;
+      max-width: 80px;
+    }
   }
 }
 </style>
